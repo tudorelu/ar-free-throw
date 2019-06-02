f4c07bac06b9f328001688aa1764a349d0f1508c
Tudor <tudor@Nuruls-MBP.lan>
2019-06-02 15:40:18 +1000

    Added the Code for ARFreeThrow

---
+++ Assets/ARFreeThrow/Scripts/BallControl.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallControl : MonoBehaviour
+{
+	// This is the force of the throw
+	public float m_ThrowForce = 100f;
+
+	// X and Y axis damping factors for the throw direction
+	public float m_ThrowDirectionX = 0.17f;
+	public float m_ThrowDirectionY = 0.67f;
+
+	// Offset of the ball's position in relation to camera's position
+	public Vector3 m_BallCameraOffset = new Vector3(0f, -1.4f, 3f);
+
+	// The following variables contain the state of the current throw
+	private Vector3 startPosition;
+	private Vector3 direction;
+	private float startTime;
+	private float endTime;
+	private float duration;
+	private bool directionChosen = false;
+	private bool throwStarted = false;
+
+	[SerializeField]
+	GameObject ARCam;
+
+	[SerializeField]
+	ARSessionOrigin m_SessionOrigin;
+
+	Rigidbody rb;
+
+	private void Start(){
+		rb = gameObject.GetComponent<Rigidbody>();
+		m_SessionOrigin = GameObject.Find("AR Session Origin").GetComponent<ARSessionOrigin>();
+		ARCam = m_SessionOrigin.transform.Find("AR Camera").gameObject;
+		transform.parent = ARCam.transform;
+		ResetBall();
+	}
+
+	private void Update(){
+
+		// We've started the touch of the screen, which will start collecting info about the ball throw
+		if(Input.GetMouseButtonDown(0)){ // Works for both Mouse and Touch on Mobile, when we press/touch
+			startPosition = Input.mousePosition;
+			startTime = Time.time;
+			throwStarted = true;
+			directionChosen = false;
+		} 
+		// We've ended the touch of the screen, which will end collecting info about the ball throw
+		else if (Input.GetMouseButtonUp(0)) { // Works for both Mouse and Touch, when we release click/touch
+			endTime = Time.time;
+			duration = endTime - startTime;
+			direction = Input.mousePosition - startPosition;
+			directionChosen = true;
+		}
+
+		// Direction was chosen, which will release/throw the ball
+		if (directionChosen) {
+			rb.mass = 1;
+			rb.useGravity = true;
+
+			rb.AddForce(
+				ARCam.transform.forward * m_ThrowForce / duration +
+				ARCam.transform.up * direction.y * m_ThrowDirectionY +
+				ARCam.transform.right * direction.x * m_ThrowDirectionX);
+
+			startTime = 0.0f;
+			duration = 0.0f;
+
+			startPosition = new Vector3(0, 0, 0);
+			direction = new Vector3(0, 0, 0);
+
+			throwStarted = false;
+			directionChosen = false;
+		}
+
+		// 5 seconds after throwing the ball, we reset it's position
+		if(Time.time - endTime >= 5 && Time.time - endTime <= 6)
+			ResetBall();
+
+	}
+
+	public void ResetBall(){
+		rb.mass = 0;
+		rb.useGravity = false;
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = Vector3.zero;
+		endTime = 0.0f;
+
+		Vector3 ballPos = ARCam.transform.position + ARCam.transform.forward * m_BallCameraOffset.z + ARCam.transform.up * m_BallCameraOffset.y;
+		transform.position = ballPos;
+	}
+
+}
---
+++ Assets/ARFreeThrow/Scripts/PlaceHoop.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceHoop : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this hoop prefab on a plane at the touch location.")]
+    GameObject m_HoopPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedHoop
+    {
+        get { return m_HoopPrefab; }
+        set { m_HoopPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedHoop { get; private set; }
+
+    [SerializeField]
+    [Tooltip("Instantiates this ball prefab in front of the AR Camera.")]
+    GameObject m_BallPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedBall
+    {
+        get { return m_BallPrefab; }
+        set { m_BallPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedBall { get; private set; }
+
+    /// <summary>
+    /// Invoked whenever an object is placed in on a plane.
+    /// </summary>
+    public static event Action onPlacedObject;
+
+    private bool isPlaced = false;
+
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+    	if(isPlaced)
+    		return;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = s_Hits[0].pose;
+
+                    spawnedHoop = Instantiate(m_HoopPrefab, hitPose.position, Quaternion.AngleAxis(180, Vector3.up));
+                    spawnedHoop.transform.parent = transform.parent;
+
+                    isPlaced = true;
+
+                    spawnedBall = Instantiate(m_BallPrefab);
+                    spawnedBall.transform.parent = m_RaycastManager.transform.Find("AR Camera").gameObject.transform;
+
+                    if (onPlacedObject != null)
+                    {
+                        onPlacedObject();
+                    }
+                }
+            }
+        }
+    }
+}
